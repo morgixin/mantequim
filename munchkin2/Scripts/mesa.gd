61134505f1427e27f424fe1d4e74385481729fd4
Rafael Amparo <rafa110304@gmail.com>
2024-12-30 00:15:13 -0300

    esqueci de salvar tudo que mexi kkkk

--- munchkin2/Scripts/mesa.gd
+++ munchkin2/Scripts/mesa.gd
@@ -11,11 +11,9 @@ func _unhandled_key_input(event: InputEvent) -> void:
 	else:
 		pass
 		
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
