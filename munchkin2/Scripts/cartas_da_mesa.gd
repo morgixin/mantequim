c2b254a6a078d24020c000c6911e88bfe13ea610
João Gabriel Lima Marinho <jg.limamarinho202@gmail.com>
2024-12-30 18:47:47 -0300

    fix: Highlight card issue

--- munchkin2/Scripts/cartas_da_mesa.gd
+++ munchkin2/Scripts/cartas_da_mesa.gd
@@ -73,10 +73,12 @@ func stop_drag():
 		cardBeingDragged = null
 
 func connect_card_signals(card):
+	print("connect")
 	card.connect("hovered", hovered_on_card)
 	card.connect("hoveredOff", hovered_off_card)
 	
 func hovered_on_card(card):
+	print("hover")
 	if !isHoveringOnCard:
 		isHoveringOnCard = true
 		highlight_card(card, true)
@@ -85,12 +87,14 @@ func hovered_off_card(card):
 	if !cardBeingDragged:
 		highlight_card(card,false)
 		var newCardHovered = raycast_check_for_card()
+		print(newCardHovered)
 		if newCardHovered:
 			highlight_card(newCardHovered, true)
 		else:
 			isHoveringOnCard = false
 	
 func highlight_card(card, hovered):
+	print("highlight")
 	if playerHandReference.maoJogador.has(card):
 		if hovered:
 			card.scale = Vector2(1.05, 1.05)
